c78a0f85e091bf3c4d6a0b55dd26eeef0b98431f
bumgyu <soul1902@naver.com>
2023-02-21 20:10:13 +0900

    객체와  테이블 매핑, DB스키마 자동 생성, 필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,15 +19,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //jpql
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(5)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("성이름");
+            member.setRoleType(RoleType.GUEST);
+
+            em.persist(member);
 
             /**
              * 생성
