fc9a2ac75590a9dc77028a500a27693364c847c7
bumgyu <soul1902@naver.com>
2023-02-21 12:40:30 +0900

    Hello JPA - 애플리케이션 개발(Persistence.xml(하이버네이트 옵션 주석), persistence -> EntityManagerFactory -> EntityManager -> Transaction -> 필요한 로직-> commit -> close)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,14 +2,68 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //persistence -> EntityManagerFactory -> EntityManager
+        //EntityManagerFactory 는 로딩 시점에 한 개만 생성 되어야 한다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager entityManager = emf.createEntityManager();
+
+        //행위가 있을 때 마다 EntityManager를 만들어줘야 한다.
+        EntityManager em = emf.createEntityManager();
+
+        //데이터가 변경되는 단위는 트랜잭션을 꼭 해주어야 한다.
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //jpql
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(5)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            /**
+             * 생성
+             * Member member = new Member();
+             * member.setId(1L);
+             * member.setName("HelloA");
+             * em.persist(member);
+             */
+
+             /**
+             * 조회
+             * Member findMember = em.find(Member.class, 1L);
+             * System.out.println("findMember.getId() = " + findMember.getId());
+             * System.out.println("findMember.getName() = " + findMember.getName());
+             */
+
+            /**
+             * 삭제
+             * Member findMember = em.find(Member.class, 1L);
+             * em.remove(findMember);
+             */
+
+            /**
+             * 수정
+             * Member findMember = em.find(Member.class, 2L);
+             * findMember.setName("HelloC");
+             */
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         //code
-        entityManager.close();
         emf.close();
     }
 }
